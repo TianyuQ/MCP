adbf10d63b8f28b4023221b43c9584bd0cef4b05
TianyuQ <tianyuqiu@utexas.edu>
2025-04-08 19:23:58 -0500

    bug fix in jacobian

--- examples/test_receding_horizon.jl
+++ examples/test_receding_horizon.jl
@@ -18,13 +18,13 @@ function rank_array_from_large_to_small(array)
     return sorted_indices
 end
 
-function mask_computation(input_traj, trajectory, mode, sim_step, mode_parameter)
+function mask_computation(input_traj, trajectory, control, mode, sim_step, mode_parameter)
     mask = zeros(N-1)
     if mode == "All"
         mask = ones(N-1)
     elseif mode == "Neural Network Threshold"
         if sim_step <= 10
-            mask = mask_computation(input_traj, trajectory, "Distance Threshold", sim_step, 2)
+            mask = mask_computation(input_traj, trajectory, control, "Distance Threshold", sim_step, 2)
         else
             mask = best_model(input_traj)
             # println("Pred Mask: ", round.(mask, digits=4))
@@ -54,7 +54,7 @@ function mask_computation(input_traj, trajectory, mode, sim_step, mode_parameter
         end
     elseif mode == "Neural Network Rank"
         if sim_step <= 10
-            mask = mask_computation(input_traj, trajectory, "Nearest Neighbor", sim_step, mode_parameter)
+            mask = mask_computation(input_traj, trajectory, control, "Nearest Neighbor", sim_step, mode_parameter)
         else
             model_mask = best_model(input_traj)
             ranked_indices = rank_array_from_large_to_small(model_mask)
@@ -63,6 +63,30 @@ function mask_computation(input_traj, trajectory, mode, sim_step, mode_parameter
                 mask[ranked_indices[i]] = 1
             end
         end
+    elseif mode == "Jacobian"
+        if sim_step == 1
+            mask = mask_computation(input_traj, trajectory, control, "Nearest Neighbor", sim_step, mode_parameter)
+        else
+            mask = zeros(N-1)
+            delta_t = 0.1 # hard coded for now
+            norm_costs = zeros(N-1)
+            for player_id in 2:N
+                state_differences = (trajectory[1][end-3:end] - trajectory[player_id][end-3:end]) # ex: pi_{k,x} - pj_{k,x}
+                delta_px = (state_differences[1] + delta_t * state_differences[3]) ^ 2 # ex: (pi_{k,x} - pj_{k,x} + delta_t * (vi_{k,x} - vj_{k,x})) ^ 2
+                delta_py = (state_differences[2] + delta_t * state_differences[4]) ^ 2
+                delta_vx = (state_differences[3] + delta_t * control[player_id][1]) ^ 2
+                delta_vy = (state_differences[4] + delta_t * control[player_id][2]) ^ 2
+                D = delta_px + delta_py + delta_vx + delta_vy # denominator of l_col between player i and j = player_id
+                J1 = 1/(D ^ 2) * 2 * delta_vx * delta_t # partial derivative of l_col with respect to aj_{k,x}
+                J2 = 1/(D ^ 2) * 2 * delta_vy * delta_t # partial derivative of l_col with respect to aj_{k,y}
+                norm_costs[player_id-1] = norm([J1, J2]) # [J1, J2] is the jacobian of the cost function with respect to the control of player_id
+            end
+            ranked_indices = rank_array_from_large_to_small(norm_costs) # rank the players based on the norm of the jacobian
+            for i in 1:mode_parameter-1
+                mask[ranked_indices[i]] = 1
+            end
+        end
+        
     else
         error("Invalid mode: $mode")
     end
@@ -77,8 +101,8 @@ end
 println("\nLoading best model for testing...")
 # Use the same record_name as in training
 # best_model_data = BSON.load("/home/tq877/Tianyu/player_selection/MCP/examples/logs/$record_name/trained_model.bson")
-# best_model_data = BSON.load("/home/tq877/Tianyu/player_selection/MCP/examples/logs/bs_16 _ep_100 _lr_0.01 _sd_3 _pat_100 _N_4 _h_30 _ih10 _isd_4/trained_model.bson")
-best_model_data = BSON.load("C:/UT Austin/Research/MCP/examples/logs/bs_32 _ep_100 _lr_0.01 _sd_3 _pat_100 _N_4 _h_30 _ih10 _isd_4/trained_model.bson")
+best_model_data = BSON.load("/home/tq877/Tianyu/player_selection/MCP/examples/logs/bs_16 _ep_100 _lr_0.01 _sd_3 _pat_100 _N_4 _h_30 _ih10 _isd_4/trained_model.bson")
+# best_model_data = BSON.load("C:/UT Austin/Research/MCP/examples/logs/bs_32 _ep_100 _lr_0.01 _sd_3 _pat_100 _N_4 _h_30 _ih10 _isd_4/trained_model.bson")
 best_model = best_model_data[:model]
 println("Best model loaded successfully!")
 
@@ -100,14 +124,12 @@ for mode in evaluation_modes
                 receding_horizon_result["Player $player_id Goal"] = goals[2 * (player_id - 1) + 1:2 * player_id]
                 receding_horizon_result["Player $player_id Control"] = []
             end
+            latest_control = []
             receding_horizon_result["Player 1 Mask"] = []
             for sim_step in 1:50
                 input_traj = []
-                # println("Sim Step: ", sim_step)
                 if sim_step <= 10
-                #     current_mask = mask_computation(input_traj, trajectory, mode, sim_step)
                     if sim_step > 1
-                #         # Update the trajectory for each player
                         for player_id in 1:N
                             trajectory[player_id] = vcat(trajectory[player_id], initial_states[4 * (player_id - 1) + 1:4 * player_id])
                         end
@@ -116,7 +138,7 @@ for mode in evaluation_modes
                     trajectory = Dict(player_id => vcat(trajectory[player_id][5:end], reshape(initial_states[4 * (player_id - 1) + 1:4 * player_id], :)) for player_id in 1:N)
                     input_traj = vec(vcat([reshape(trajectory[player_id], :) for player_id in 1:N]...))
                 end
-                current_mask = mask_computation(input_traj, trajectory, mode, sim_step, mode_parameter)
+                current_mask = mask_computation(input_traj, trajectory, latest_control, mode, sim_step, mode_parameter)
                 pred_mask = vcat([1], current_mask)
                 push!(receding_horizon_result["Player 1 Mask"], pred_mask)
                 target = [0 for _ in 1:N * horizon * d]
@@ -132,13 +154,12 @@ for mode in evaluation_modes
                     target = target,
                     save = true
                 )
-                # Update initial_states for the next iteration in the desired format
                 initial_states = vec(vcat([results["Player $player_id Latest Initial State"] for player_id in 1:N]...))
                 for player_id in 1:N
                     push!(receding_horizon_result["Player $player_id Trajectory"], results["Player $player_id Latest Initial State"])
                     push!(receding_horizon_result["Player $player_id Control"], results["Player $player_id Latest Control"])
-                end
-                # println("Initial States: ", initial_states)
+                    push!(latest_control, results["Player $player_id Latest Control"])
+                end              
             end
 
             # results_path = "/home/tq877/Tianyu/player_selection/MCP/examples/logs/$record_name/similarity_safety_scores_$mode.json"
